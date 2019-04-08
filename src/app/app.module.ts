fdacd5125e35d2ae905104c400eda6411819a897
Drew Thompson <drew@commongenius.com>
2019-04-08 14:54:40 -0700

    refactor(demo): initialize app with demo id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AgoraConfig, NgxAgoraModule } from 'ngx-agora';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgxAgoraModule } from 'ngx-agora';
+
+const agoraConfig: AgoraConfig = {
+  AppID: '9cf6e81e7b204ac3ab4cd0a8988b21ed'
+};
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgxAgoraModule.forRoot({ AppID: '' })],
+  imports: [BrowserModule, AppRoutingModule, NgxAgoraModule.forRoot(agoraConfig)],
   providers: [],
   bootstrap: [AppComponent]
 })
