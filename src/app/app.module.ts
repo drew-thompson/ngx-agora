2d3656dfd543ab5ee86e925c95aec34935cffb76
Drew Thompson <drew@commongenius.com>
2019-04-10 10:51:56 -0700

    chore(demo): reset app.module for schematics testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,14 @@ import { MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/ma
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AgoraConfig, NgxAgoraModule } from 'ngx-agora';
-
-import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-const agoraConfig: AgoraConfig = {
-  AppID: environment.appId
-};
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxAgoraModule.forRoot(agoraConfig),
     BrowserAnimationsModule,
     ReactiveFormsModule,
     MatFormFieldModule,
