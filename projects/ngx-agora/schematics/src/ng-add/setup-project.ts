adc025e31db9d5400de23a42a719407eba4b0e50
Drew Thompson <drew@commongenius.com>
2019-04-09 21:08:17 -0700

    refactor(schematics): add environment config and module insertion

--- projects/ngx-agora/schematics/src/ng-add/setup-project.ts
+++ projects/ngx-agora/schematics/src/ng-add/setup-project.ts
@@ -1,17 +1,25 @@
+import { bold, red } from '@angular-devkit/core/src/terminal';
 import { chain, Rule, SchematicContext, SchematicsException, Tree } from '@angular-devkit/schematics';
-import { getProjectFromWorkspace } from '@angular/cdk/schematics';
-import { getSourceNodes } from '@schematics/angular/utility/ast-utils';
+import {
+  addModuleImportToRootModule,
+  getProjectFromWorkspace,
+  getProjectMainFile,
+  hasNgModuleImport
+} from '@angular/cdk/schematics';
+import { getSourceNodes, insertImport, isImported } from '@schematics/angular/utility/ast-utils';
+import { InsertChange } from '@schematics/angular/utility/change';
 import { getWorkspace } from '@schematics/angular/utility/config';
+import { getAppModulePath } from '@schematics/angular/utility/ng-ast-utils';
 import { SourceFile } from 'typescript';
 import { getProjectEnvironmentFile } from '../util/project-environment-file';
 import { ts } from '../util/version-agnostic-typescript';
 import { Schema } from './schema';
 
 export default function(options: Schema): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
+  return (tree: Tree, context: SchematicContext) => {
     return chain([addEnvironmentConfig(options), importEnvironemntIntoRootModule(options), addNgxAgoraModule(options)])(
       tree,
-      _context
+      context
     );
   };
 }
@@ -22,28 +30,33 @@ function addEnvironmentConfig(options: Schema): Rule {
     const project = getProjectFromWorkspace(workspace, options.project);
     const envPath = getProjectEnvironmentFile(project);
 
-    // verify environment.ts file exists
+    // Verify environment.ts file exists
     if (!envPath) {
-      return context.logger.warn(`❌ Could not find environment file: "${envPath}". Skipping firebase configuration.`);
+      return context.logger.warn(`❌ Could not find environment file: "${envPath}". Skipping ngx-agora configuration.`);
     }
 
-    // firebase config to add to environment.ts file
+    // ngx-agora config to add to environment.ts file
     const insertion = ',\n' + `\tagora: {\n` + `\t\tappId: '${options.appId}',\n` + `\t}`;
     const sourceFile = readIntoSourceFile(tree, envPath);
 
-    // verify firebase config does not already exist
+    // Verify ngx-agora config does not already exist
     const sourceFileText = sourceFile.getText();
     if (sourceFileText.includes(insertion)) {
       return;
     }
 
-    // get the array of top-level Node objects in the AST from the SourceFile
+    // Get the array of top-level Node objects in the AST from the SourceFile
     const nodes = getSourceNodes(sourceFile as any);
-    const start = nodes.find(node => node.kind === ts.SyntaxKind.OpenBraceToken)!;
-    const end = nodes.find(node => node.kind === ts.SyntaxKind.CloseBraceToken, start.end)!;
+    const start = nodes.find(node => node.kind === ts.SyntaxKind.OpenBraceToken);
+    let end;
+    if (start) {
+      end = nodes.find(node => node.kind === ts.SyntaxKind.CloseBraceToken, start.end);
+    }
 
     const recorder = tree.beginUpdate(envPath);
-    recorder.insertLeft(end.pos, insertion);
+    if (end) {
+      recorder.insertLeft(end.pos, insertion);
+    }
     tree.commitUpdate(recorder);
 
     context.logger.info('✅️ Environment configuration');
@@ -52,13 +65,51 @@ function addEnvironmentConfig(options: Schema): Rule {
 }
 
 function addNgxAgoraModule(options: Schema): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
+  return (tree: Tree, context: SchematicContext) => {
+    const MODULE_NAME = 'NgxAgoraModule.forRoot(environment.agora.appId)';
+    const workspace = getWorkspace(tree);
+    const project = getProjectFromWorkspace(workspace, options.project);
+    const appModulePath = getAppModulePath(tree, getProjectMainFile(project));
+
+    // verify module has not already been imported
+    if (hasNgModuleImport(tree, appModulePath, MODULE_NAME)) {
+      return console.warn(red(`Could not import "${bold(MODULE_NAME)}" because "${bold(MODULE_NAME)}" is already imported.`));
+    }
+
+    // add NgModule to root NgModule imports
+    addModuleImportToRootModule(tree, MODULE_NAME, 'ngx-agora', project);
+
+    context.logger.info('✅️ Import NgxAgoraModule into root module');
     return tree;
   };
 }
 
 function importEnvironemntIntoRootModule(options: Schema): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
+  return (tree: Tree, context: SchematicContext) => {
+    const IMPORT_IDENTIFIER = 'environment';
+    const workspace = getWorkspace(tree);
+    const project = getProjectFromWorkspace(workspace, options.project);
+    const appModulePath = getAppModulePath(tree, getProjectMainFile(project));
+    const envPath = getProjectEnvironmentFile(project);
+    const sourceFile = readIntoSourceFile(tree, appModulePath);
+
+    if (isImported(sourceFile as any, IMPORT_IDENTIFIER, envPath)) {
+      context.logger.info('✅️ The environment is already imported in the root module');
+      return tree;
+    }
+
+    const change = insertImport(
+      sourceFile as any,
+      appModulePath,
+      IMPORT_IDENTIFIER,
+      envPath.replace(/\.ts$/, '')
+    ) as InsertChange;
+
+    const recorder = tree.beginUpdate(appModulePath);
+    recorder.insertLeft(change.pos, change.toAdd);
+    tree.commitUpdate(recorder);
+
+    context.logger.info('✅️ Import environment into root module');
     return tree;
   };
 }
