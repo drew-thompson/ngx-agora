ce2c267436dde6c9d5428a7c3bd0543eac25e8d5
Drew Thompson <drew.thompson@segment.com>
2020-04-21 21:00:31 -0700

    feat(*): upgrade project to Angular v8

--- projects/ngx-agora/schematics/src/util/project-configurations.ts
+++ projects/ngx-agora/schematics/src/util/project-configurations.ts
@@ -1,9 +1,18 @@
-import { WorkspaceProject } from '@angular-devkit/core/src/workspace';
+import { experimental } from '@angular-devkit/core';
 
-export function getProjectTargetConfigurations(project: WorkspaceProject, buildTarget = 'build') {
-  if (project.architect && project.architect[buildTarget] && project.architect[buildTarget].options) {
+export function getProjectTargetConfigurations(
+  project: experimental.workspace.WorkspaceProject,
+  buildTarget = 'build'
+) {
+  if (
+    project.architect &&
+    project.architect[buildTarget] &&
+    project.architect[buildTarget].options
+  ) {
     return project.architect[buildTarget].configurations;
   }
 
-  throw new Error(`Cannot determine project target configurations for: ${buildTarget}.`);
+  throw new Error(
+    `Cannot determine project target configurations for: ${buildTarget}.`
+  );
 }
--- projects/ngx-agora/schematics/src/util/project-environment-file.ts
+++ projects/ngx-agora/schematics/src/util/project-environment-file.ts
@@ -1,8 +1,10 @@
-import { WorkspaceProject } from '@angular-devkit/core/src/workspace';
+import { experimental } from '@angular-devkit/core';
 import { SchematicsException } from '@angular-devkit/schematics';
 import { getProjectTargetConfigurations } from './project-configurations';
 
-export function getProjectEnvironmentFile(project: WorkspaceProject): string {
+export function getProjectEnvironmentFile(
+  project: experimental.workspace.WorkspaceProject
+): string {
   const configurations = getProjectTargetConfigurations(project, 'build');
 
   if (
@@ -10,11 +12,16 @@ export function getProjectEnvironmentFile(project: WorkspaceProject): string {
     !configurations.production.fileReplacements ||
     configurations.production.fileReplacements.length === 0
   ) {
-    throw new SchematicsException(`Could not find the configuration of the workspace config (${project.sourceRoot})`);
+    throw new SchematicsException(
+      `Could not find the configuration of the workspace config (${project.sourceRoot})`
+    );
   }
 
-  const fileReplacements: [{ replace: string; with: string }] = configurations.production.fileReplacements;
-  const fileReplacement = fileReplacements.find(replacement => /environment\.ts$/.test(replacement.replace));
+  const fileReplacements: [{ replace: string; with: string }] =
+    configurations.production.fileReplacements;
+  const fileReplacement = fileReplacements.find((replacement) =>
+    /environment\.ts$/.test(replacement.replace)
+  );
 
   if (fileReplacement === undefined) {
     throw new SchematicsException(
